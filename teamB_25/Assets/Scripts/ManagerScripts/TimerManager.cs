5fb1438bf5bae7db87e2533d3bdfc67deaae2462
e1q24062@oit.ac.jp <e1q24062@oit.ac.jp>
2025-05-14 18:14:50 +0900

    タイマーを作った

--- teamB_25/Assets/Scripts/ManagerScripts/TimerManager.cs
+++ teamB_25/Assets/Scripts/ManagerScripts/TimerManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TimerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float startTimer = 180f;
+    private float nowTimer;
 
-    // Update is called once per frame
+    public TextMeshProUGUI timeText;
+
+    private void Start()
+    {
+        SetTimer();
+    }
+    private void SetTimer()
+    {
+        nowTimer = startTimer;
+    }
     void Update()
     {
-        
+        Timer();
+        TimerText();
+    }
+    /// <summary>
+    /// 制限時間の計算
+    /// </summary>
+    private void Timer()
+    {
+        nowTimer -= Time.deltaTime;
+        //制限時間経過
+        if (nowTimer<=0f)
+        {
+            //SceneManager.LoadScene("END");
+        }
+    }
+    /// <summary>
+    /// 残り時間表記 
+    /// </summary>
+    private void TimerText()
+    {
+        int minute = (int)nowTimer / 60;
+        int second = (int)nowTimer % 60;
+        timeText.text = $"{minute:00}:{second:00}";
     }
 }
