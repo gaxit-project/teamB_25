cd218385289b68056e9a6405d5a73ed39651a97a
LAPTOP-TTDPT3KH\ssaku <ssakuraa3@gmail.com>
2025-05-24 00:33:32 +0900

    2025/05/24 森本 スクリプトにコメントつけた

--- teamB_25/Assets/Scripts/ManagerScripts/StartTimer.cs
+++ teamB_25/Assets/Scripts/ManagerScripts/StartTimer.cs
@@ -4,7 +4,7 @@ using TMPro;
 
 public class StartTimer : MonoBehaviour
 {
-    public static bool IsGameStarted { get; private set; } = false;
+    public static bool IsGameStarted { get; private set; } = false; // ゲームが開始されたかのフラグ
 
     public Canvas StartCanvas;
     public TextMeshProUGUI startCountText;
@@ -19,12 +19,14 @@ public class StartTimer : MonoBehaviour
     
     void Update()
     {
+        // カウントダウン進行中の処理
         if(countdownTime > 0)
         {
             TimerManager.countdownActive = false; //TimerManagerを止める
             countdownTime -= Time.deltaTime;
             startCountText.text = Mathf.Ceil(countdownTime).ToString(); 
         }
+        // カウントダウン終了後の処理
         else if(countdownTime <= 0)
         {
             StartPlay();
@@ -33,14 +35,18 @@ public class StartTimer : MonoBehaviour
         }
     }
 
+    // ゲームを開始する処理
     void StartPlay()
     {
         Debug.Log("Start!");
+
+        // ゲーム内の他のコンポーネントを動作させる
         TimerManager.countdownActive = true;
         PlayerBase.countdownActive = true;
         IsGameStarted = true;
     }
 
+    // テキストを１秒表示したのち非表示にする処理
     IEnumerator WaitErase()
     {
         yield return new WaitForSeconds(1f);
