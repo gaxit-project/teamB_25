af33aca435541baa2fe06bd0fed035b10e3823d8
e1b24064 <e1b24064@oit.ac.jp>
2025-06-10 20:46:32 +0900

    2025/06/10 辻 状態ごとに恐竜のアニメーションが変化するようになった。

--- teamB_25/Assets/Scripts/DinosaurScript/DinosaurAnimationManager.cs
+++ teamB_25/Assets/Scripts/DinosaurScript/DinosaurAnimationManager.cs
@@ -28,6 +28,10 @@ public class DinosaurAnimationManager : MonoBehaviour
 
     public void PlayLeap()
     {
+        // Yé≤180ìxâÒì]Åiåªç›ÇÃX,ZÇÕï€éùÅj
+        Vector3 currentEulerAngles = transform.eulerAngles;
+        transform.eulerAngles = new Vector3(currentEulerAngles.x, 180f, currentEulerAngles.z);
+
         animator.Play("Leap");
     }
 
--- teamB_25/Assets/Scripts/DinosaurScript/NormalDinosaur.cs
+++ teamB_25/Assets/Scripts/DinosaurScript/NormalDinosaur.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-#if UNITY_EDITOR
-[CustomEditor(typeof(Dinosaur_Base))]
-#endif
-
 public class NormalDinosaur : Dinosaur_Base
 {
     
