0c8142a73f633b2b5b66432efad63c783057395c
LAPTOP-TTDPT3KH\ssaku <ssakuraa3@gmail.com>
2025-07-09 15:39:26 +0900

    2025/07/09 森本 breaker調整

--- teamB_25/Assets/Scripts/ManagerScripts/breaker.cs
+++ teamB_25/Assets/Scripts/ManagerScripts/breaker.cs
@@ -1,23 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using System.Text.RegularExpressions; // Regexを使うために必要
 
 public class Breaker : MonoBehaviour
 {
-    
     private bool isCollision = false;
     public bool isActivated = false;
-    
+    public string sceneToMuteSE = "Introduction";
+
+    // Mainシーンのみ音を出す
+    private void Start()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (currentSceneName != sceneToMuteSE)
+        {
+            AudioManager.Instance.PlaySELoop("BrokenBreaker", transform);
+        }
+    }
 
     public void bootBreaker()
     {
+        AudioManager.Instance.DestroySE("BrokenBreaker");
         AudioManager.Instance.PlaySE("BreakerOn", transform.position);
         isActivated = true;
 
         // ブレーカー名からエリア番号を取得
         string breakerName = gameObject.name;
-        string number = System.Text.RegularExpressions.Regex.Replace(breakerName, "[^0-9]", "");
+        string number = Regex.Replace(breakerName, "[^0-9]", ""); // System.Text.RegularExpressions.Regex を using で追加済み
 
         // 対応するライト親オブジェクトを探す
         GameObject lightParent = GameObject.Find("light" + number);
@@ -37,7 +49,8 @@ public class Breaker : MonoBehaviour
             outline.OutlineColor = Color.green;
         }
 
-        //GetComponent<Renderer>().material.color = Color.green; // 色を変える
+        AudioManager.Instance.PlaySE("ReBreaker", transform.position);
+
         BreakerManager.Instance.ActivateBreaker();
     }
 }
